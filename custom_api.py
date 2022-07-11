29ba4c469bf5082ffeb71c0a1a729a1dd23d546b
Arnav Andraskar <arnavandraskar8@gmail.com>
2022-07-11 22:06:31 +0530

    Update custom_api.py

--- custom_api.py
+++ custom_api.py
@@ -2,11 +2,21 @@ import requests
 class speed_limit:
     def __init__(self,api_key):
         self.key = api_key
+        self.avail = []
+        self.data  = None
+
+    def get_data(self,coord = []):
+        if list(self.avail) != list(coord):
+            self.avail = coord
+            api_url = "https://routematching.hereapi.com/v8/match/routelinks?apikey={key}&waypoint0={lat},{long}&mode=fastest;car&routeMatch=1&attributes=SPEED_LIMITS_FCn(FROM_REF_SPEED_LIMIT,TO_REF_SPEED_LIMIT)".format(key = self.key ,lat=coord[0],long = coord[1])
+            r = requests.get(url = api_url ) 
+            self.data = r.json()
+            return self.data
+        else:
+            return self.data
         
-    def get_speed_limit(self,lat = None ,long = None):
-        api_url = "https://routematching.hereapi.com/v8/match/routelinks?apikey={key}&waypoint0={lat},{long}&mode=fastest;car&routeMatch=1&attributes=SPEED_LIMITS_FCn(*)".format(key = self.key ,lat=lat,long = long)
-        r = requests.get(url = api_url ) 
-        data = r.json()
+    def get_limit(self,coord = []):
+        data = self.get_data(coord)
         try:
             to_loc = data['response']['route'][0]['leg'][0]['link'][0]['attributes']['SPEED_LIMITS_FCN'][0]['TO_REF_SPEED_LIMIT']
             from_loc = data['response']['route'][0]['leg'][0]['link'][0]['attributes']['SPEED_LIMITS_FCN'][0]['FROM_REF_SPEED_LIMIT']
