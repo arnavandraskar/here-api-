ac89dfb1ec62c72293687971bc8ce55c5e447b29
Arnav Andraskar <arnavandraskar8@gmail.com>
2022-07-11 22:21:38 +0530

    Update custom_api.py

--- custom_api.py
+++ custom_api.py
@@ -4,17 +4,22 @@ class speed_limit:
         self.key = api_key
         self.avail = []
         self.data  = None
-
+        
+    #Input to this function must be list/1d-array having single latitude and longitude   
+    #eg: coord  = [latitude,longitude]
     def get_data(self,coord = []):
         if list(self.avail) != list(coord):
             self.avail = coord
-            api_url = "https://routematching.hereapi.com/v8/match/routelinks?apikey={key}&waypoint0={lat},{long}&mode=fastest;car&routeMatch=1&attributes=SPEED_LIMITS_FCn(FROM_REF_SPEED_LIMIT,TO_REF_SPEED_LIMIT)".format(key = self.key ,lat=coord[0],long = coord[1])
+            api_url = "https://routematching.hereapi.com/v8/match/routelinks?apikey={key}&waypoint0={lat},{long}&mode=fastest;car&routeMatch=1&\
+                       attributes=SPEED_LIMITS_FCn(FROM_REF_SPEED_LIMIT,TO_REF_SPEED_LIMIT)".format(key = self.key ,lat=coord[0],long = coord[1])
             r = requests.get(url = api_url ) 
             self.data = r.json()
             return self.data
         else:
             return self.data
         
+    #Input to this function must be list/1d-array having single latitude and longitude    
+    #eg: coord  = [latitude,longitude]   
     def get_limit(self,coord = []):
         data = self.get_data(coord)
         try:
